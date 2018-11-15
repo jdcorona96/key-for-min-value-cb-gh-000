8796456d4fbf43038d246cc48cd5e58dd7d48c00
Joseph Corona <josed.hc96@gmail.com>
2018-11-15 03:11:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,7 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     return nil
   end
-  min = name_hash[0]
-binding.pry
+  min = -9999999999999999
   ret = ""
   name_hash.each do |key,val|
     if val < min
